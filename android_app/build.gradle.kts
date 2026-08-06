aa18ed7c405eb223bb1be44beb83c4747989f166
Emmanuel Ludwaro <youngtrippy119@gmail.com>
2026-08-06 14:33:51 +0300

    Update build.gradle.kts

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    // Modern Kotlin/Android essentials
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.google.ksp) apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
 }
